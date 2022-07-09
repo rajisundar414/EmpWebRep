ea011cc6bcfe082d7760ce0e9f1d4234e4d45d8d
Rajalakshmi <rajisundar414@gmail.com>
2022-07-09 19:39:16 +0530

    Adding Controller and Model

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebEmpApp.Models;
+
+namespace WebEmpApp.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Emp> lstEmployees = new List<Emp>
+            {
+                new Emp{EId=1,EName="Raji",EDesing="Programmer",EDOJ=System.DateTime.Parse("12/06/2022")},
+                new Emp{EId=1,EName="Lakshmi",EDesing="Manager",EDOJ=System.DateTime.Parse("09/06/2020")},
+                new Emp{EId=1,EName="Roopan",EDesing="Manager",EDOJ=System.DateTime.Parse("12/08/2018")},
+                new Emp{EId=1,EName="Suba",EDesing="Programmer",EDOJ=System.DateTime.Parse("06/06/2021")},
+
+            };
+            return View(lstEmployees);
+        }
+    }
+}
--- Models/Emp.cs
+++ Models/Emp.cs
@@ -1,6 +1,13 @@
-﻿namespace WebEmpApp.Models
+﻿using System;
+
+namespace WebEmpApp.Models
 {
     public class Emp
     {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesing { get; set; }
+        public DateTime EDOJ { get; set; }
     }
 }
+
